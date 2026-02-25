65755eca10aebe481f8a97974a2925f194c93889
IamRyoKA <piyaphatvlog2547@gmail.com>
2026-02-26 03:50:10 +0700

    Search page UserInterface#1 Putter

--- Front-End/app.py
+++ Front-End/app.py
@@ -2,20 +2,17 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+# # แบบที่ 1 query ปกติ
+# @app.route('/search1')
+# def search_book1():
+#     return render_template('search_book1.html')
 
+# # แบบที่ 2 query ทุกครั้งที่พิมพ์
+# @app.route('/search2')
+# def search_book2():
+#     return render_template('search_book2.html')
 
-# แบบที่ 1 query ปกติ
-@app.route('/search1')
-def search_book1():
-    return render_template('search_book1.html')
-
-# แบบที่ 2 query ทุกครั้งที่พิมพ์
-@app.route('/search2')
-def search_book2():
-    return render_template('search_book2.html')
-
-# !!!!!!!!! ส่วนนี้คือการจำลองจาก backend โปรดสื่อสารกัน !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+# !!!!!!!!! ส่วนนี้คือการจำลองจาก backend !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # สมมติฐานข้อมูลจำลอง
 books = [
     {"id": 1, "title": "Python 101", "author": "John Doe"},
@@ -23,20 +20,26 @@ books = [
     {"id": 3, "title": "Data Science Basics", "author": "Alex Lee"},
 ]
 
+@app.route('/search')
+def search_book2():
+    user_data = {
+        "firstName": "First Name", 
+        "profileImage": None       
+    }
+    return render_template('search_book.html', user=user_data)
+
 @app.route('/api/search')
-def book_query2():
+def book_query():
     query = request.args.get('q', '').lower()
+    if not query:
+        return jsonify([])
     
-    # Filter logic
+ 
     results = [
-        book for book in books 
+        book for book in books_db 
         if query in book['title'].lower() or query in book['author'].lower()
     ]
-    
-    # Return data as JSON
     return jsonify(results)
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 if __name__ == '__main__':
     app.run(debug=True)
