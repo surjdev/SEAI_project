b70bfcc509fcbb493c104e1268fcd8d5626c07f3
Pitsanatj <pjunsingto@gmail.com>
2026-03-01 00:04:52 +0700

    connect database

--- Back-End/Database/database.py
+++ Back-End/Database/database.py
@@ -2,11 +2,27 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from contextlib import contextmanager
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+@contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
