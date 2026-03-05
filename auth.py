6f0faa01b156ba656f2bf1f0f01104c2c339c45f
rpiyaphat1 <piyaphatvlog2547@gmail.com>
2026-03-05 21:38:38 +0700

    authentication system

--- auth.py
+++ auth.py
@@ -1,15 +1,11 @@
 import os
-from dotenv import load_dotenv
 from flask import Blueprint, url_for, redirect, session
 from authlib.integrations.flask_client import OAuth
 from flask_login import login_user, logout_user
 
-load_dotenv()
-
 auth_bp = Blueprint('auth', __name__)
 oauth = OAuth()
 
-# Initialize Google OAuth
 google = oauth.register(
     name='google',
     client_id=os.getenv('GOOGLE_CLIENT_ID'),
@@ -21,7 +17,6 @@ google = oauth.register(
 @auth_bp.route('/login')
 def login():
     redirect_uri = url_for('auth.callback', _external=True)
-    print(f"DEBUG: Redirect URI being sent to Google is: {redirect_uri}")
     return google.authorize_redirect(redirect_uri)
 
 @auth_bp.route('/callback')
@@ -29,11 +24,14 @@ def callback():
     token = google.authorize_access_token()
     user_info = token.get('userinfo')
     
-    # In a real app, you'd save this user to your Database here
-    # For now, we'll create a simple User object for Flask-Login
-    from app import User
-    user = User(id=user_info['sub'], name=user_info['name'])
-    login_user(user)
+    if user_info:
+        session['user_id'] = user_info['sub']
+        session['user_name'] = user_info['name']  
+        session['user_email'] = user_info['email']
+        
+        from app import User
+        user = User(id=user_info['sub'], name=user_info['name'])
+        login_user(user)
     
     return redirect('/')
 
